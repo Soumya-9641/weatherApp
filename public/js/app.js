80518a5cf0d4e97cb25bda73eeaa113272024f02
Soumyadip <soumyadipgantait2003@gmail.com>
2023-01-13 14:52:14 +0530

    finalize all html page using hbs

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+const submitBtn = document.getElementById('submitbtn');
+const getInfo=(e)=>{
+    //e.preventDefault();
+    console.log("Hiiii")
+    alert("Hiiii");
+} 
+submitBtn.addEventListener('click',getInfo);
